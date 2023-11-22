441f0c92db401dd7fd68a2be4362017a1ead79bf
simo.zgaoua <m.zgaoua2001@gmail.com>
2023-11-22 21:03:38 +0100

    show categories

--- app/src/main/java/ma/ensaf/mybookmanagerapplication/DashboardAdminActivity.kt
+++ app/src/main/java/ma/ensaf/mybookmanagerapplication/DashboardAdminActivity.kt
@@ -1,9 +1,13 @@
 package ma.ensaf.mybookmanagerapplication
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import ma.ensaf.mybookmanagerapplication.databinding.ActivityDashboardAdminBinding
 
 class DashboardAdminActivity : AppCompatActivity() {
@@ -11,13 +15,23 @@ class DashboardAdminActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDashboardAdminBinding
     //firebase auth
     private lateinit var firebaseAuth: FirebaseAuth
+
+    //arrayList to hold categories
+    private lateinit var categoryArrayList: ArrayList<ModelCategory>
+    //adapter
+    private lateinit var adapterCategory: AdapterCategory
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardAdminBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         //init firebase Auth
         firebaseAuth = FirebaseAuth.getInstance()
         checkUser()
+        loadCategories()
+
+        //search
+
 
         //handle click, Logout
         binding.logoutBtn.setOnClickListener {
@@ -30,6 +44,33 @@ class DashboardAdminActivity : AppCompatActivity() {
             startActivity(Intent(this,CategoryAddActivity::class.java))
         }
 
+    }
+
+    private fun loadCategories() {
+        //init arraylist
+        categoryArrayList = ArrayList()
+        //get all categories from firebase database... Firebase DB > Categories
+        val ref = FirebaseDatabase.getInstance().getReference ("Categories")
+        ref.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //clear list before starting addding data into it
+                categoryArrayList.clear()
+                for (ds in snapshot.children){
+                    //get data as model
+                    val model = ds.getValue(ModelCategory::class.java)
+                    //add to arraylist
+                    categoryArrayList.add(model!!)
+                }
+                //setup adapter
+                adapterCategory = AdapterCategory(this@DashboardAdminActivity, categoryArrayList)
+                //set adapter to recylerview
+                binding.categoriesRV.adapter = adapterCategory
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 
     private fun checkUser() {
