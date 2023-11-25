73df73581daab88f4933cc460d325679cd589927
AyZedAitch <adam.houlihal@usmba.ac.ma>
2023-11-25 20:42:53 +0100

    Show books admin UI

--- app/src/main/java/ma/ensaf/mybookmanagerapplication/AdapterCategory.kt
+++ app/src/main/java/ma/ensaf/mybookmanagerapplication/AdapterCategory.kt
@@ -2,6 +2,7 @@ package ma.ensaf.mybookmanagerapplication
 
 import android.app.AlertDialog
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -63,6 +64,12 @@ class AdapterCategory : RecyclerView.Adapter<AdapterCategory.HolderCategory> , F
                 }
                 .show()
 
+        }
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, PdfListAdminActivity::class.java)
+            intent.putExtra("categoryId", id)
+            intent.putExtra("category", category)
+            context.startActivity(intent)
         }
     }
 
