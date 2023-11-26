7c7728906bd4a0853de2018da77cce3e9178aec8
simo.zgaoua <m.zgaoua2001@gmail.com>
2023-11-26 23:17:12 +0100

    Book options dialog

--- app/src/main/java/ma/ensaf/mybookmanagerapplication/AdapterPdfAdmin.kt
+++ app/src/main/java/ma/ensaf/mybookmanagerapplication/AdapterPdfAdmin.kt
@@ -1,6 +1,8 @@
 package ma.ensaf.mybookmanagerapplication
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -66,6 +68,39 @@ class AdapterPdfAdmin :RecyclerView.Adapter<AdapterPdfAdmin.HolderPdfAdmin>, Fil
         //Load pdf size
         MyApplication.LoadPdfSize(pdfUrl, title, holder.sizeTv)
 
+        //handle click, show dialog with options 1) Edit Book, 2) Delete Book
+        holder.moreBtn.setOnClickListener {
+            moreOptionsDialog (model, holder)
+        }
+
+    }
+
+    private fun moreOptionsDialog(model: ModelPdf, holder: AdapterPdfAdmin.HolderPdfAdmin) {
+        //get id, url, title of book
+        val bookId= model.id
+        val bookUrl = model.url
+        val bookTitle = model.title
+
+        //options to show in dialog
+        val options = arrayOf("Edit", "Delete")
+
+        //alert dialog
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("Choose Option")
+            .setItems(options){dialog, position->
+                //handle item click
+                if(position == 0){
+                    //edit is clicked
+                    val intent = Intent(context, PdfEditActivity::class.java)
+                    intent.putExtra("bookId", bookId) //passed bookId, will be used to edit the book
+                    context.startActivity(intent)
+                }
+                else if(position == 1){
+                    //delete is clicked
+
+                }
+            }
+            .show()
     }
 
     override fun getFilter(): Filter {
