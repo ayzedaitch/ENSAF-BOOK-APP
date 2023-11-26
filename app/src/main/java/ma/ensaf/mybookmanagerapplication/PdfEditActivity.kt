fa2928f4a9ddbe69609a530d6c0c5e9274440b54
simo.zgaoua <m.zgaoua2001@gmail.com>
2023-11-27 00:23:59 +0100

    load book categories

--- app/src/main/java/ma/ensaf/mybookmanagerapplication/PdfEditActivity.kt
+++ app/src/main/java/ma/ensaf/mybookmanagerapplication/PdfEditActivity.kt
@@ -1,11 +1,83 @@
 package ma.ensaf.mybookmanagerapplication
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import ma.ensaf.mybookmanagerapplication.databinding.ActivityPdfEditBinding
 
 class PdfEditActivity : AppCompatActivity() {
+
+    //view binding
+    private lateinit var binding: ActivityPdfEditBinding
+
+    private companion object{
+        private const val TAG = "PDF_EDIT_TAG"
+    }
+
+
+    //book id get from intent started from Adapter Pdf Admin
+    private var bookId=""
+
+    //progress dialog
+    private lateinit var progressDialog: ProgressDialog
+
+    //arraylist to hold category titles
+    private lateinit var categoryTitleArrayList: ArrayList<String>
+
+    //arraylist to hold category ids
+    private lateinit var categoryIdArrayList: ArrayList<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pdf_edit)
+        binding = ActivityPdfEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //get book id to edit the book info
+        bookId= intent.getStringExtra("bookId")!!
+
+        //setup progress dialog
+        progressDialog = ProgressDialog( this)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        loadCategories()
+    }
+
+    private fun loadCategories() {
+        Log.d(TAG,"LoadCategories: loading categories...")
+
+        categoryTitleArrayList = ArrayList()
+        categoryIdArrayList = ArrayList()
+        val ref = FirebaseDatabase.getInstance().getReference("Categories")
+        ref.addListenerForSingleValueEvent(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //clear list before starting adding data into them
+                categoryIdArrayList.clear()
+                categoryTitleArrayList.clear()
+
+                for (ds in snapshot.children) {
+
+                    val id="${ds.child("id").value}"
+                    val category="${ds.child("category").value}"
+
+                    categoryIdArrayList.add(id)
+                    categoryTitleArrayList.add(category)
+
+                    Log.d(TAG,"onDataChange: Category ID $id")
+                    Log.d(TAG,"onDataChange: Category $category")
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
